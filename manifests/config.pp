dd7241110d4a91adb017b9a38e9b9547d0304915
Marc Villacorta <marc.villacorta@gmail.com>
2014-05-11 21:30:12 +0200

    Adding supervisord.d directory

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,11 +1,19 @@
 class supervisord::config inherits supervisord {
 
-    file { '/etc/supervisord.conf':
-        ensure  => present,
-        content => template("${module_name}/supervisord.conf.erb"),
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0644',
-        require => Package['supervisor'],
+    file {
+
+        '/etc/supervisord.conf':
+            ensure  => present,
+            content => template("${module_name}/supervisord.conf.erb"),
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0644',
+            require => Package['supervisor'];
+
+        '/etc/supervisord.d':
+            ensure => directory,
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0755';
     }
 }
