d6cc80893b3a02a2bfa3c4dd9af39a12786a5b31
Marc Villacorta <marc.villacorta@gmail.com>
2014-05-22 21:51:33 +0200

    Install supervisord via setuptools and pip

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,4 +1,18 @@
 class supervisord::install inherits supervisord {
 
-    package { 'supervisor': ensure => $version }
+    package { 'python-setuptools':
+        ensure => $version,
+    } ->
+
+    exec { 'easy_install_pip':
+        command => 'easy_install pip',
+        creates => '/usr/bin/pip',
+        path    => '/usr/bin',
+    } ->
+
+    exec { 'pip_install_supervisor':
+        command => 'pip install supervisor',
+        creates => '/usr/bin/supervisord',
+        path    => '/usr/bin',
+    }
 }
