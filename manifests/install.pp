28ddbd270425ddd9774c9602a8ff73b847d4af78
Marc Villacorta <marc.villacorta@gmail.com>
2014-05-24 00:44:57 +0200

    Adding package python-setuptools

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,11 +1,14 @@
 class supervisord::install inherits supervisord {
 
+    package { 'python-setuptools':
+        ensure => latest,
+    } ->
+
     package { 'python-pip':
         ensure => latest,
     } ->
 
     package { 'supervisor':
-        ensure   => $version,
         provider => 'pip',
     }
 }
